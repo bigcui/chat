b01b7a18f1a7c02df094b769b97acdeff0630b83
cuihonglei <cuihonglei@baidu.com>
2016-01-13 21:38:38 +0800

    test.html

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,68 +1,79 @@
 function divEscapedContentElement(message) {
-    return $('<div></div>').text(message);
+  return $('<div></div>').text(message);
 }
 
-function divSystemConcentElement(message) {
-    return $("<div></div>").html('<i> ' + message + '</i>');
+function divSystemContentElement(message) {
+  return $('<div></div>').html('<i>' + message + '</i>');
 }
 
 function processUserInput(chatApp, socket) {
-    var message = $('#message').val();
-    var systemMessage;
-    if (message.chatAt(0) == '/') {
-        systemMessage = chatApp.processCommand(message);
-        if (systemMessage) {
-            $('#messages').append(divSystemConcentElement(systemMessage));
-        }
-    } else {
-        chatApp.sendFile($('#room').text(), message);
-        $('#message').append(divEscapedContentElement(message));
-        $('#message').scrollTop($('#message').prop('scrollHeight'));
+  var message = $('#send-message').val();
+  var systemMessage;
+
+  if (message.charAt(0) == '/') {
+    systemMessage = chatApp.processCommand(message);
+    if (systemMessage) {
+      $('#messages').append(divSystemContentElement(systemMessage));
     }
-    $("#send-message").val();
+  } else {
+    chatApp.sendMessage($('#room').text(), message);
+    $('#messages').append(divEscapedContentElement(message));
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+  }
+
+  $('#send-message').val('');
 }
 
-//   客户端
 var socket = io.connect();
-$(dicument).ready(function() {
-    var chatApp = new chat('socket');
-    socket.on('nameResult', function(result) {
-        var message;
-        if (result.success) {
-            message = 'you are now know as ' + result.name + '.';
-        } else {
-            message = result.message;
-        }
-        $("#message").append(divSystemConcentElement(message));
-    });
-    socket.on('jionResult', function(result) {
-        $('#room').text(result.room);
-        $('#message').append(divEscapedContentElement('room changed.'));
-    });
 
-    socket.on('message', function(message) {
-        var newElement = $('<div></div>').text(message.text);
-        $('#message').append(newElement);
-    });
-    socket.on('rooms', function(rooms) {
-        $('#room-list').empty();
-        for (var room in rooms) {
-            room = room.substring(1, room.length);
-            if (room != '') {
-                $('#room-list').append(divEscapedContentElement(room));
-            }
-        }
-    });
+$(document).ready(function() {
+  var chatApp = new Chat(socket);
+
+  socket.on('nameResult', function(result) {
+    var message;
+
+    if (result.success) {
+      message = 'You are now known as ' + result.name + '.';
+    } else {
+      message = result.message;
+    }
+    $('#messages').append(divSystemContentElement(message));
+  });
+
+  socket.on('joinResult', function(result) {
+    $('#room').text(result.room);
+    $('#messages').append(divSystemContentElement('Room changed.'));
+  });
+
+  socket.on('message', function (message) {
+    var newElement = $('<div></div>').text(message.text);
+    $('#messages').append(newElement);
+  });
+
+  socket.on('rooms', function(rooms) {
+    $('#room-list').empty();
+
+    for(var room in rooms) {
+      room = room.substring(1, room.length);
+      if (room != '') {
+        $('#room-list').append(divEscapedContentElement(room));
+      }
+    }
+
     $('#room-list div').click(function() {
-        chatApp.processCommand('/join' + $(this).text());
-        $('#send-message').focus();
+      chatApp.processCommand('/join ' + $(this).text());
+      $('#send-message').focus();
     });
-});
-setIntever(function() {
+  });
+
+  setInterval(function() {
     socket.emit('rooms');
-}, 1000);
-$('#send-message]').focus();
-$('#send-form').submit(function() {
+  }, 1000);
+
+  $('#send-message').focus();
+
+  $('#send-form').submit(function() {
     processUserInput(chatApp, socket);
     return false;
+  });
 });
